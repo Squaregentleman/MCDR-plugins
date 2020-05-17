a52aed79646581a6425d1bc53cea8f8841a24c29
方块君 <2991965574@qq.com>
2020-05-17 16:49:22 +0800

    Update gamemode.py

--- gamemode.py
+++ gamemode.py
@@ -4,62 +4,74 @@ import re
 import time
 import os
 
-gm_user = 0
-logfile = './plugins/gm/'
+def on_player_joined(server, player):
+  if os.path.exists('./plugins/gm/' + player):
+    server.execute("gamemode spectator " + player)
 
-
-def on_info(server, info):
-  global gm_user
-  global user
-  dimension_convert = {"0":"overworld","-1":"the_nether","1":"the_end"}
-  if (info.is_player == 0):
-    if("following entity data" in info.content):
-      if gm_user>0:
-        gm_user-=1
-        name = info.content.split(" ")[0]
-        if user == name:
-          server.tell(name, '§6已获取到数据，正在更改模式!')
-          dimension = re.search("(?<=Dimension: )-?\d",info.content).group()
-          position_str = re.search("(?<=Pos: )\[.*?\]",info.content).group()
-          position = re.findall("\[(-?\d*).*?, (-?\d*).*?, (-?\d*).*?\]",position_str)[0]
-          #os.system('cd plugins/gm && echo "'+str(position[0])+'|'+str(position[1])+'|'+str(position[2])+'|'+dimension_convert[dimension]+'" > ' + name)
-          f = open('./plugins/gm/' + name, 'w')
-          f.write(str(position[0])+'|'+str(position[1])+'|'+str(position[2])+'|'+dimension_convert[dimension])
-          f.close()
-          server.execute("gamemode spectator " + name)
-          user = ''
-          server.tell(name, '§6已切换到观察者模式,要切换回来请!!gm')
-  else:
-    per = server.get_permission_level(info)
-    if info.content.startswith('!!gm') and info.is_player == 1:
-      if per >= 0:
-        gm = info.content.split(" ")
-        if os.path.exists('./plugins/gm/' + info.player):
-          server.tell(info.player, '§65秒后改变模式,请不要走动!')
-          time.sleep(5)
-          server.tell(info.player, '§6正在更改模式，请不要走动!')
-          server.tell(info.player, '§6更改时间由服务器tps决定!')
-          f = open('plugins/gm/' + info.player, 'r')
-          zub = f.read()
-          zub = zub.replace('\n', '').replace('\r', '')
-          f.close()
-          os.remove('plugins/gm/' + info.player)
-          xyz = zub.split("|")
-          server.execute("execute at " + info.player + " in minecraft:" + xyz[3] + " run tp " + info.player + " " + xyz[0] + " " + str(int(xyz[1]) + 0.5) + " " + xyz[2])
-          server.execute("gamemode survival " + info.player)
-        else:
-          server.tell(info.player, '§65秒后改变模式,请不要走动!')
-          time.sleep(5)
-          server.tell(info.player, '§6正在更改模式，请不要走动!')
-          server.tell(info.player, '§6更改时间由服务器tps决定!')
-          gm_user+=1
-          os.system('cd plugins/gm && echo " " > ' + info.player)
-          user = info.player
-          server.execute("data get entity "+info.player)
-      else:
-        server.tell(info.player, '你没有权限')
+def getPlayerInfo(server, name):
+	if hasattr(server, 'MCDR'):
+		api = server.get_plugin_instance('PlayerInfoAPI')
+	else:
+		api = load_source('PlayerInfoAPI', './plugins/PlayerInfoAPI.py')
+	return api.getPlayerInfo(server, name)
 
 def on_load(server, old):
     server.add_help_message('!!gm', '切换玩家模式')
-    if not os.path.exists(logfile):
-      os.makedirs(logfile)
+    server.add_help_message('!!gm [ID]', '管理员强制切换切换玩家模式(限制权限admin)')
+    if not os.path.exists('./plugins/gm/'):
+      os.makedirs('./plugins/gm/')
+
+def on_info(server, info):
+  dimension_convert = {"0":"overworld","-1":"the_nether","1":"the_end"}
+  per = server.get_permission_level(info)
+  if info.content.startswith('!!gm') and info.is_player == 1:
+    if per >= 1:
+      gm = info.content.split(" ")
+      if len(gm) == 2 and per == 3:
+          server.tell(info.player, '即将更改玩家 §6{0} §r的 §a游戏模式!'.format(gm[1]))
+          if os.path.exists('./plugins/gm/' + gm[1]):
+            server.tell(gm[1], '§6正在更改模式，请不要走动!')
+            time.sleep(1)
+            f = open('plugins/gm/' + gm[1], 'r')
+            zub = f.read()
+            zub = zub.replace('\n', '').replace('\r', '')
+            f.close()
+            os.remove('plugins/gm/' + gm[1])
+            xyz = zub.split("|")
+            server.execute("execute at " + gm[1] + " in minecraft:" + xyz[3] + " run tp " + gm[1] + " " + xyz[0] + " " + xyz[1] + " " + xyz[2])
+            server.execute("gamemode survival " + gm[1])
+          else:
+            server.tell(gm[1], '§6正在更改模式，请不要走动!')
+            time.sleep(1)
+            os.system('cd plugins/gm && echo " " > ' + gm[1])
+            player_info = getPlayerInfo(server, gm[1])
+            f = open('./plugins/gm/' + gm[1], 'w')
+            f.write(str(player_info['Pos'][0])+'|'+str(player_info['Pos'][1])+'|'+str(player_info['Pos'][2])+'|'+dimension_convert[str(player_info['Dimension'])])
+            f.close()
+            server.execute("gamemode spectator " + gm[1])
+            server.tell(gm[1], '§6已切换到观察者模式,要切换回来请!!gm')
+      elif len(info.content) != 4:
+        server.tell(info.player, '§6装你妈的B')
+      elif os.path.exists('./plugins/gm/' + info.player):
+        server.tell(info.player, '§6正在更改模式，请不要走动!')
+        time.sleep(1)
+        f = open('plugins/gm/' + info.player, 'r')
+        zub = f.read()
+        zub = zub.replace('\n', '').replace('\r', '')
+        f.close()
+        os.remove('plugins/gm/' + info.player)
+        xyz = zub.split("|")
+        server.execute("execute at " + info.player + " in minecraft:" + xyz[3] + " run tp " + info.player + " " + xyz[0] + " " + xyz[1] + " " + xyz[2])
+        server.execute("gamemode survival " + info.player)
+      else:
+        server.tell(info.player, '§6正在更改模式，请不要走动!')
+        time.sleep(1)
+        os.system('cd plugins/gm && echo " " > ' + info.player)
+        player_info = getPlayerInfo(server, info.player)
+        f = open('./plugins/gm/' + info.player, 'w')
+        f.write(str(player_info['Pos'][0])+'|'+str(player_info['Pos'][1])+'|'+str(player_info['Pos'][2])+'|'+dimension_convert[str(player_info['Dimension'])])
+        f.close()
+        server.execute("gamemode spectator " + info.player)
+        server.tell(info.player, '§6已切换到观察者模式,要切换回来请!!gm')
+    else:
+      server.tell(info.player, '你没有权限')
